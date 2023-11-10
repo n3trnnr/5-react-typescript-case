67efbd8fae2653211a6bc1eb6395501dc73978de
n3trnnr <n3trnnr@gmail.com>
2023-11-10 03:29:57 +0300

    Updates

---
+++ precache-manifest.c7f093cfa89c80d8291b60669de756b3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4d91038091b0cb4679ffa94793f0d0",
+    "url": "/5-react-typescript-task/index.html"
+  },
+  {
+    "revision": "59e50a4f90ad59965915",
+    "url": "/5-react-typescript-task/static/css/main.cf819014.chunk.css"
+  },
+  {
+    "revision": "a09d5d5fde0623d95724",
+    "url": "/5-react-typescript-task/static/js/2.ca86ebca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/5-react-typescript-task/static/js/2.ca86ebca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59e50a4f90ad59965915",
+    "url": "/5-react-typescript-task/static/js/main.94240949.chunk.js"
+  },
+  {
+    "revision": "ef8241e21c7a792ca28c",
+    "url": "/5-react-typescript-task/static/js/runtime-main.2419bd62.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/5-react-typescript-task/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/5-react-typescript-task/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/5-react-typescript-task/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/5-react-typescript-task/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/5-react-typescript-task/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/5-react-typescript-task/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/5-react-typescript-task/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/5-react-typescript-task/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/5-react-typescript-task/static/media/font.f01e8d15.woff"
+  }
+]);
